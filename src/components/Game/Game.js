571f58baac3f74ffa026c950618f101c09b7cc67
liyiz <5160472+liyiz@users.noreply.github.com>
2026-01-12 11:48:58 -0500

    Exercise 1 review

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import GuessInput from '../GuessInput';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+
+import GuessInput from '../GuessInput';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,36 +1,40 @@
 import React from 'react';
-import '../../styles.css';
 
 function GuessInput() {
 
   const [guess, setGuess] = React.useState('');
+
+  function handleSubmit() {
+    console.log({guess});
+    if (guess.length !== 5) {
+      alert('You did not submit a 5 letter guess. Please try again.');
+      return;
+    }
+    // checkGuess
+    setGuess(''); // clear input value after checking guess
+  }
 
   return (
     <form 
       className="guess-input-wrapper"
       onSubmit={(event) => {
         event.preventDefault();
-
-        const nextGuess = {};
-        nextGuess.guess = guess.toUpperCase();
-
-        console.log(nextGuess);
-        // checkGuess
-
-        setGuess(''); // clear input value after checking guess
+        handleSubmit();
       }}
     >
       <label htmlFor="guess-input">{'Enter guess:'}</label>
       <input 
+        required
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}" /* RegEx to only allow 5 alphabet characters exactly */
         id="guess-input" 
         type="text" 
-        pattern="[a-zA-Z]{5}" /* RegEx to only allow 5 alphabet characters exactly */
         value={guess}
-        onChange={
-          (event) => {
-            setGuess(event.target.value);
-          }
-        }
+        onChange={ (event) => {
+          const nextGuess = event.target.value.toUpperCase();
+          setGuess(nextGuess);
+        }}
       />
     </form>
   );
