030139deeeb49145fe0984abc0fad195d7f75c39
liyiz <5160472+liyiz@users.noreply.github.com>
2026-01-16 15:47:07 -0500

    Exercise 4 review addendum

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,18 +2,29 @@ import React from 'react';
 import { range } from '../../utils';
 import { checkGuess } from '../../game-helpers';
 
+function Cell( {letter, className} ) {
+  return (          
+    <span className={className} key={ crypto.randomUUID() }>
+      {letter}
+    </span>
+  )
+}
+
 function Guess( {value, answer} ) {
 
   const result = checkGuess(value, answer);
+  console.log(result);
 
   return (
     <p className="guess">
       { range(5).map((num) => {
         const classToApply = value ? 'cell ' + result[num].status : 'cell';
         return (
-          <span className={classToApply} key={ crypto.randomUUID() }>
-            {result ? result[num].letter : undefined}
-          </span>
+          <Cell 
+            letter={result ? result[num].letter : undefined} 
+            className={classToApply} 
+            key={crypto.randomUUID()} 
+          />
         )
       })}
     </p>
